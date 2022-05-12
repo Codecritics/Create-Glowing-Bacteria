946e3d28594972329c8acbb427c3573438d585da
Tanawa Tsamo Marius <mariustanawa@gmail.com>
2022-05-12 22:05:59 +0200

    Stage 1/6: A complementary strand

--- bacteria.py
+++ bacteria.py
@@ -1,1 +1,12 @@
-# write your program here
+origin_strand = input()
+complementary_strand = ""
+complementary_principle = {
+    "A": "T",
+    "T": "A",
+    "C": "G",
+    "G": "C"
+}
+for vitamin in origin_strand:
+    complementary_strand = "".join([complementary_strand, complementary_principle[vitamin]])
+
+print(complementary_strand)
