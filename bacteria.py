067d7d1f8b8e1482aaca563ab5a4901fd6c2bf40
Tanawa Tsamo Marius <mariustanawa@gmail.com>
2022-05-16 10:12:08 +0200

    Stage 2/6: Cut the plasmid

--- bacteria.py
+++ bacteria.py
@@ -1,12 +1,17 @@
-origin_strand = input()
-complementary_strand = ""
+origin_strand, complementary_strand = input().split()
+
+origin_start = origin_strand.rindex("CTGCAG")
+origin_restriction_site = origin_strand[origin_start:origin_start + 6]
+complementary_restriction_site = ""
 complementary_principle = {
     "A": "T",
     "T": "A",
     "C": "G",
     "G": "C"
 }
-for vitamin in origin_strand:
-    complementary_strand = "".join([complementary_strand, complementary_principle[vitamin]])
+for nucleotide in origin_restriction_site:
+    complementary_restriction_site = "".join([complementary_restriction_site, complementary_principle[nucleotide]])
 
-print(complementary_strand)
+print(origin_strand[:origin_start + 1], origin_strand[origin_start + 1:])
+complementary_start = complementary_strand.rindex(complementary_restriction_site)
+print(complementary_strand[:complementary_start + 5], complementary_strand[complementary_start + 5:])
