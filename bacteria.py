6df9c3532153bc327b8c0629ce303f23cf704113
Tanawa Tsamo Marius <mariustanawa@gmail.com>
2022-05-19 21:07:56 +0200

    Stage 3/6: A GFP strand

--- bacteria.py
+++ bacteria.py
@@ -1,17 +1,16 @@
-origin_strand, complementary_strand = input().split()
+def get_complementary_strand(origin: str) -> str:
+    complementary_strand = ""
+    complementary_principle = {
+        "A": "T",
+        "T": "A",
+        "C": "G",
+        "G": "C"
+    }
+    for vitamin in origin:
+        complementary_strand = "".join([complementary_strand, complementary_principle[vitamin]])
+    return complementary_strand
 
-origin_start = origin_strand.rindex("CTGCAG")
-origin_restriction_site = origin_strand[origin_start:origin_start + 6]
-complementary_restriction_site = ""
-complementary_principle = {
-    "A": "T",
-    "T": "A",
-    "C": "G",
-    "G": "C"
-}
-for nucleotide in origin_restriction_site:
-    complementary_restriction_site = "".join([complementary_restriction_site, complementary_principle[nucleotide]])
 
-print(origin_strand[:origin_start + 1], origin_strand[origin_start + 1:])
-complementary_start = complementary_strand.rindex(complementary_restriction_site)
-print(complementary_strand[:complementary_start + 5], complementary_strand[complementary_start + 5:])
+if __name__ == '__main__':
+    origin_strand = input()
+    print(origin_strand, get_complementary_strand(origin_strand), sep="\n")
